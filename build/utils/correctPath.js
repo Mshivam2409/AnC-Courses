d3bf6688407db81ef7d1fc5a403a106726ea9795
Mshivam2409 <shivammalhotra08@gmail.com>
2020-12-07 12:19:37 +0530

    Delete Cache

--- build/utils/correctPath.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const correctPath = (path) => {
-    return `${process.env.NODE_ENV === "development" ? "src" : "."}/${path}`;
-};
-exports.default = correctPath;
-//# sourceMappingURL=correctPath.js.map
