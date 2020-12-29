29c9be4a92462c0fc45feae3a1c1a219a415d347
Mshivam2409 <shivammalhotra08@gmail.com>
2020-12-29 11:46:14 +0530

    Removed Typescript Server

--- server/src/backup.ts
+++
@@ -1,7 +0,0 @@
-import low from "lowdb"
-import FileSync from "lowdb/adapters/FileSync"
-import correctPath from "utils/correctPath"
-
-const adapter = new FileSync(correctPath("backup/backupDB.json"))
-const backupDB = low(adapter)
-export default backupDB
--- server/src/utils/correctPath.ts
+++
@@ -1,7 +0,0 @@
-import { PathLike } from "fs"
-
-const correctPath = (path: string | PathLike) => {
-    return `${process.env.NODE_ENV === "development" ? "src" : "."}/${path}`
-}
-
-export default correctPath
